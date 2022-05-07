0d95facc313dbf676acbb63a733a15e06224f761
PBillingsby <philbillingsby@gmail.com>
2022-05-06 17:35:43 -0700

    add bundlr wallet fund. add image form

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
 import '../styles/globals.css'
+import { MainContext } from '../context'
+import { useState, useRef } from 'react'
+
+import { providers, utils } from 'ethers'
+import { WebBundlr } from '@bundlr-network/client'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [bundlrInst, setBundlrInst] = useState();
+  const [balance, setBalance] = useState();
+  const bundlrRef = useRef()
+
+  const initialize = async () => {
+    await window.ethereum.enable();
+    const provider = new providers.Web3Provider(window.ethereum);
+    await provider._ready();
+
+    const bundlr = new WebBundlr('https://node1.bundlr.network', 'matic', provider)
+    await bundlr.ready();
+
+    setBundlrInst(bundlr);
+    bundlrRef.current = bundlr;
+    getBalance();
+  }
+
+  const getBalance = async () => {
+    const bal = await bundlrRef.current.getLoadedBalance()
+    setBalance(utils.formatEther(bal.toString()))
+  }
+
+  return (
+    <div className='container'>
+      <MainContext.Provider value={{
+        initialize,
+        getBalance,
+        balance,
+        bundlrInst
+      }}>
+        <h1>Arweave/Bundlr</h1>
+        <Component {...pageProps} />
+      </MainContext.Provider>
+    </div>
+  )
 }
 
 export default MyApp
